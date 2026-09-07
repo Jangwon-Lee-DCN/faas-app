08f17f61d97b8a9f1822798aefe55a96d7d48225
Jangwon-Lee-DCN <jangown.lee@dcn.ssu.ac.kr>
2026-09-07 06:14:01 +0000

    Limit random traffic concurrency to 20

--- run-random-traffic.sh
+++ run-random-traffic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  random_number=$((RANDOM % 100 + 1))
+  random_number=$((RANDOM % 20 + 1))
   ./load-test-ue.sh "$random_number" "$@"
   sleep $((RANDOM % 10 + 1))
 done
